4be3dd1cc172b883e26d16577f07f0ea9cfaff28
Jack Gamboa <jgamboa929@gmail.com>
2020-03-22 07:22:43 -0500

    Loan calculator complete.

--- 04-DOM-projects/02-loan-calculator/script.js
+++ 04-DOM-projects/02-loan-calculator/script.js
@@ -1,10 +1,18 @@
 // Event listeners
-document
-  .querySelector("#loan-form")
-  .addEventListener("submit", calculateResults);
+document.querySelector("#loan-form").addEventListener("submit", e => {
+  // Hide results
+  document.getElementById("results").style.display = "none";
+
+  // Show loader
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e) {
+function calculateResults() {
   const amount = document.querySelector("#amount");
   const interest = document.querySelector("#interest");
   const years = document.querySelector("#years");
@@ -36,9 +44,49 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
-  } else {
-    console.log("Please check your numbers");
-  }
 
-  e.preventDefault();
+    // Show results
+    document.getElementById("results").style.display = "block";
+
+    // Hide loader
+    document.getElementById("loading").style.display = "none";
+  } else {
+    showError("Please check your numbers.");
+  }
+}
+
+// Show Error
+function showError(error) {
+  // Hide results
+  document.getElementById("results").style.display = "none";
+
+  // Show loader
+  document.getElementById("loading").style.display = "none";
+
+  // Create error element
+  const errorDiv = document.createElement("div");
+
+  // Get elements
+  const card = document.querySelector(".card"); // parent element
+  const heading = document.querySelector(".heading"); // child element
+
+  // Add Bootstrap class
+  errorDiv.className = "alert alert-danger";
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  // parentElement.insertBefore(newChild, refChild);
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after a few seconds
+  window.setTimeout(clearError, 3000);
+}
+
+// Clear error
+function clearError() {
+  // errorDiv.style.display = "none";
+
+  document.querySelector(".alert").remove();
 }
