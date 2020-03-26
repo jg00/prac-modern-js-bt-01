19f399963120bb0554914708d0f1eb4c95d728ad
Jack Gamboa <jgamboa929@gmail.com>
2020-03-26 11:20:04 -0500

    Add, remove item to list.

--- 06-book-list-project/book-list/app.js
+++ 06-book-list-project/book-list/app.js
@@ -8,9 +8,67 @@ function Book(title, author, isbn) {
 // UI Constructor - for set of prototype methods like add book to the list, delete the book, show alerts, etc.
 function UI() {}
 
+// Add book to list
+UI.prototype.addBookToList = function(book) {
+  const list = document.getElementById("book-list");
+
+  // Create tr element
+  const row = document.createElement("tr");
+
+  // Insert columns
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class='delete'>X</a></td>`;
+
+  list.appendChild(row);
+};
+
+// Clear form values
+UI.prototype.clearFields = function() {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
+
+// Remove item
+UI.prototype.removeItem = function(e) {
+  if (e.target.className === "delete") {
+    console.log(e.target.parentElement);
+    console.log(e.target.parentElement.parentElement);
+    e.target.parentElement.parentElement.remove();
+  }
+};
+
 // Event Listeners
+// Add New Book
 document.getElementById("book-form").addEventListener("submit", function(e) {
-  console.log(e.target);
+  // Get form values
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const isbn = document.getElementById("isbn").value;
+
+  // Create book object
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI object
+  const ui = new UI();
+
+  // Add book to list
+  ui.addBookToList(book);
+
+  // Clear form values
+  ui.clearFields();
 
   e.preventDefault();
+});
+
+// Remove item from list
+document.getElementById("book-list").addEventListener("click", function(e) {
+  // Instantiate UI
+  const ui = new UI();
+
+  // Remove item
+  ui.removeItem(e);
 });
