1055ccd282c00417a0e17789049295f94f509f56
Jack Gamboa <jgamboa929@gmail.com>
2020-04-02 06:18:58 -0500

    Fetch API testing complete.

--- 07-async-projects/07-fetch-sandbox/app.js
+++ 07-async-projects/07-fetch-sandbox/app.js
@@ -1,7 +1,11 @@
 document.getElementById("button1").addEventListener("click", getText);
 
+document.getElementById("button2").addEventListener("click", getJson);
+
+document.getElementById("button3").addEventListener("click", getExternal);
+
 function getText() {
-  fetch("test2.txt")
+  fetch("test.txt")
     .then(function(res) {
       // console.log(res.text()); // Promise {<pending>}
       // console.log(res.ok); // With fetch api you have to check res.ok and accordingly throw an error to invoke .catch.
@@ -15,7 +19,7 @@ function getText() {
     })
 
     .then(function(data) {
-      console.log(data);
+      document.getElementById("output").innerHTML = `${data}`;
     })
 
     .catch(function(err) {
@@ -31,3 +35,60 @@ function getText() {
   - With fetch API (unlike axios), if there is an http error, it will not fire off .catch automatically. 
     You have to check the response and throw an error yourself.
 */
+
+function getJson() {
+  fetch("posts.json")
+    .then(function(res) {
+      if (!res.ok) {
+        throw new Error(res.error);
+      }
+      return res.json();
+    })
+    .then(function(data) {
+      let output = "";
+      data.forEach(
+        function(item) {
+          output += `<li>${item.title} ${item.body}</li>`;
+        }.bind(this)
+      );
+
+      document.getElementById("output").innerHTML = output;
+    })
+    .catch(function(err) {
+      console.log("Error:", err);
+    });
+}
+
+function getExternal() {
+  fetch("http://api.github.com/users")
+    .then(function(res) {
+      // if (!res.ok) {
+      //   throw new Error(res.error);
+      // }
+      // return res.json();
+
+      return res;
+    })
+    .then(handleErrors)
+    .then(function(data) {
+      console.log(data);
+      let output = "";
+      data.forEach(
+        function(item) {
+          output += `<li>${item.login} <img style="width:100px;display:block" src="${item.avatar_url}"></img></li>`;
+        }.bind(this)
+      );
+
+      document.getElementById("output").innerHTML = output;
+    })
+    .catch(function(err) {
+      console.log("Error:", err);
+    });
+}
+
+function handleErrors(res) {
+  // console.log(res);
+  if (!res.ok) throw new Error(res.error);
+
+  return res.json();
+}
