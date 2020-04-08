a5ae05a464bbd455ad12c8e0379dcc96b2f7ed3c
Jack Gamboa <jgamboa929@gmail.com>
2020-04-08 10:10:18 -0500

    Start weatherjs api.  UI started.

---
+++ 08-API-projects/02-WeatherJS/settings_template.js
@@ -0,0 +1,6 @@
+/*
+  1 Rename settings_template.js to settings.js.  Settings.js is referenced in index.html. 
+  2 Register for an api key at wunderground api.
+  3 Place api info below.
+*/
+const weatherjs_apiKey = "12345";
