6f83d1f0fc9cdbdd07ec22e10370d3a916e4493c
Jack Gamboa <jgamboa929@gmail.com>
2020-05-05 18:08:28 -0500

    Load items and display.

--- 12-tracalorie-project/tracalorie/app.js
+++ 12-tracalorie-project/tracalorie/app.js
@@ -1,14 +1,80 @@
-// Storage Controller
-
-// Item Controller
-
-// UI Controller
-
-// Main Controller - Initializers
-
 /*
   .item-name, .item-calories
   .add-btn, .update-btn, .delete-btn, .back-btn
   .total-calories
   #item-list, .collection, collection-item, #item-0
 */
+
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function () {
+  // Item Constructor
+  const Item = function (id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  };
+
+  // Data Structure / State
+  const data = {
+    items: [
+      { id: 0, name: "Steak Dinner", calories: 1200 },
+      { id: 1, name: "Cookie", calories: 400 },
+      { id: 2, name: "Eggs", calories: 300 },
+    ],
+    currentItem: null,
+    totalCalories: 0,
+  };
+
+  return {
+    getItems: function () {
+      return data.items;
+    },
+
+    logData: function () {
+      return data;
+    },
+  };
+})();
+
+// UI Controller
+const UICtrl = (function () {
+  const UISelectors = {
+    itemList: "#item-list",
+  };
+
+  return {
+    populateItemList: function (items) {
+      let html = "";
+
+      items.forEach(function (item) {
+        html += `<li class="collection-item" id="item-${item.id}">
+        <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+        <a href="#" class="secondary-content">
+          <i class="edit-item fa fa-pencil"></i>
+        </a>
+      </li>`;
+      });
+
+      // Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    },
+  };
+})();
+
+// App Controller - Initializers
+const App = (function (ItemCtrl, UICtrl) {
+  return {
+    init: function () {
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+      // Populate list with items
+      UICtrl.populateItemList(items);
+    },
+  };
+})(ItemCtrl, UICtrl);
+
+// Initialize App
+App.init();
