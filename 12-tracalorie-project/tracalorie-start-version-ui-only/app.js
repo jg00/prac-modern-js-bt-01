3ea4736e83a60a70b95487723bbcab9bf597808c
Jack Gamboa <jgamboa929@gmail.com>
2020-05-05 18:10:38 -0500

    UI only starting point.

---
+++ 12-tracalorie-project/tracalorie-start-version-ui-only/app.js
@@ -0,0 +1,14 @@
+/*
+  .item-name, .item-calories
+  .add-btn, .update-btn, .delete-btn, .back-btn
+  .total-calories
+  #item-list, .collection, collection-item, #item-0
+*/
+
+// Storage Controller
+
+// Item Controller
+
+// UI Controller
+
+// App Controller - Initializers
