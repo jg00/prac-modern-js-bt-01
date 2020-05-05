0a4ce10d9a6feae3c357fdf1ae6a6c37f3ffbae8
Jack Gamboa <jgamboa929@gmail.com>
2020-05-05 14:51:56 -0500

    Main page set up complete.

---
+++ 12-tracalorie-project/tracalorie/app.js
@@ -0,0 +1,14 @@
+// Storage Controller
+
+// Item Controller
+
+// UI Controller
+
+// Main Controller - Initializers
+
+/*
+  .item-name, .item-calories
+  .add-btn, .update-btn, .delete-btn, .back-btn
+  .total-calories
+  #item-list, .collection, collection-item, #item-0
+*/
