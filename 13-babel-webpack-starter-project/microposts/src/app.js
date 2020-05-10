6906e1d00dafc64dcc48c434f348fb03b24fa832
Jack Gamboa <jgamboa929@gmail.com>
2020-05-09 20:07:17 -0500

    Get posts and add post.

--- 13-babel-webpack-starter-project/microposts/src/app.js
+++ 13-babel-webpack-starter-project/microposts/src/app.js
@@ -1,4 +1,49 @@
 /*
   Utilizes JSONPlaceholder - Fake Online REST API for Testing and Prototyping
-  npm install json-server --save
+    npm install json-server --save (initial setup)
+    Create db.json (initial setup)
+    > npm run json:server (custom script)
+
+  For our app:
+    > npm start
 */
+
+import { http } from "./http";
+import { ui } from "./ui";
+
+// Get posts on DOM load
+document.addEventListener("DOMContentLoaded", getPosts);
+
+// Listen for submit post
+document.querySelector(".post-submit").addEventListener("click", submitPost);
+
+// Get posts
+function getPosts() {
+  http
+    .get("http://localhost:3000/posts")
+    .then(function (data) {
+      ui.showPosts(data);
+    })
+    .catch((err) => console.log(err));
+}
+
+// Submit post
+function submitPost() {
+  const title = document.querySelector("#title").value;
+  const body = document.querySelector("#body").value;
+
+  const data = {
+    title: title,
+    body: body,
+  };
+
+  // Create post
+  http
+    .post("http://localhost:3000/posts", data)
+    .then((data) => {
+      ui.showAlert("Post added", "alert alert-success");
+      getPosts();
+      ui.clearFields();
+    })
+    .catch((err) => console.log(err));
+}
